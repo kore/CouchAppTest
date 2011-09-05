964e0641f72e1c4de3dd277fc49366ecd588784b
Kore Nordmann <mail@kore-nordmann.de>
2011-09-05 16:39:22 +0200

    Extract method: create User

--- ifam/frontend/scripts/application.js
+++ ifam/frontend/scripts/application.js
@@ -15,7 +15,7 @@ function myQuery( url, callback, data, method )
                 throw( result );
             },
         dataType: "json",
-        contentType: "application/json",
+        contentType: "application/json"
     } );
 }
 
@@ -27,35 +27,34 @@ function refreshUserList()
         function ( data, textStatus, jqXHR ) {
             $.each( data.rows, function( key, value ) {
                 $( 'ul#users' ).append( "<li>"+ value.doc.name + " &lt;" + value.doc.email + "&gt;</li>" );
-            } )
+            } );
         }
     );
 }
 
-// @TODO: extract user create function.
+function createUser()
+{
+    // function myQuery( url, callback, data, method )
+    myQuery(
+        "/" + $( 'input[name="name"]' ).val(),
+        function ( data, textStatus, jqXHR ) {
+            console.log( data );
+            refreshUserList();
+        },
+        JSON.stringify( {
+            name: $( 'input[name="name"]' ).val(),
+            email: $( 'input[name="email"]' ).val(),
+            type: "user"
+        } ),
+        "PUT"
+    );
+
+    return false;
+}
+
 $( document ).ready( function() {
 
-    $( "#createUser" ).bind( "submit", function() {
-
-        // function myQuery( url, callback, data, method )
-        myQuery(
-            "/" + $( 'input[name="name"]' ).val(),
-            function ( data, textStatus, jqXHR ) {
-                console.log( data );
-                refreshUserList();
-            },
-            JSON.stringify( {
-                name: $( 'input[name="name"]' ).val(),
-                email: $( 'input[name="email"]' ).val(),
-                type: "user"
-            } ),
-            "PUT"
-        );
-
-        return false;
-    } );
-
+    $( "#createUser" ).bind( "submit", createUser );
     refreshUserList();
 } );
-
 
