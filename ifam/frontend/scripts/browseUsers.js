c52f6e8e0a8675674e6876cd05c956ef221fa7c5
Kore Nordmann <mail@kore-nordmann.de>
2011-09-06 15:20:31 +0200

    Implemented: Autocomplete for users

--- ifam/frontend/scripts/browseUsers.js
+++ ifam/frontend/scripts/browseUsers.js
@@ -1,7 +1,24 @@
 function showBrowseUsers()
 {
     setActive( "browseUsers" );
-    displayTemplate( "#content", "browse_users.tpl", function() {
-        // @TODO: Bind autocomplete stuff
+    displayTemplate( "#content", "browse_users.tpl", null, function() {
+        $( "#content input[name=user]" ).autocomplete( {
+            delay: 200,
+            minLength: 1,
+            source: function ( request, response ) {
+                var lastXhr = $.getJSON( '/api/_design/ifam/_view/users?startkey="' + request.term + '"&endkey="' + request.term + '\u9999"', {}, function( data, status, xhr ) {
+                    console.log( data );
+                    if ( xhr === lastXhr ) {
+                        // Refactor CouchDB return value into valid structure for the UI autocomplete widget
+                        var users = [];
+                        $.each( data.rows, function( key, value ) {
+                            users.push( value.key );
+                        } );
+
+                        response( users );
+                    }
+                } );
+            },
+        } );
     } );
 }
--- ifam/frontend/templates/browse_users.tpl
+++ ifam/frontend/templates/browse_users.tpl
@@ -1,3 +1,11 @@
 <h2>Browse users</h2>
 
+<fieldset>
+    <form>
+        <input type="text" name="user" />
+        <input type="submit" value="search" />
+    </form>
+</fieldset>
 
+<div id="user">
+</div>
